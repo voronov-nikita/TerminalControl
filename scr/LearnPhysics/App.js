3a9cdf20fe15e201360f4062b82ef8df1cf161cd
voronov <114691383+voronov-nikita@users.noreply.github.com>
2023-07-08 23:40:56 +0300

    rewritting

--- scr/LearnPhysics/App.js
+++ scr/LearnPhysics/App.js
@@ -1,17 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { SafeAreaView, StyleSheet, Text, Button, Image } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, Button, Alert, Image } from 'react-native';
 
 
-const pressButton = () => console.log('Click');
+// const pressButton = () => console.log('Click');
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
       <Text>Hello!!!</Text>
 
-      <Button title='Press Me' color='red' onPress={pressButton}/>
-
-      <Image source={require('/assets/adaptive-icon.png')} />
+      <Image source={require('./assets/adaptive-icon.png')} />
 
       <StatusBar style="auto" />
     </SafeAreaView>
@@ -21,6 +19,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    // backgroundColor: 'black',
+    // color: 'white',
   },
 });
