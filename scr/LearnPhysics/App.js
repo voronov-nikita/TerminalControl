d26abd82dbcf36370f99ec978a117dc320bba117
voronov <114691383+voronov-nikita@users.noreply.github.com>
2023-07-10 22:54:33 +0300

    new-change

--- scr/LearnPhysics/App.js
+++ scr/LearnPhysics/App.js
@@ -2,11 +2,11 @@ import React from 'react';
 import { StyleSheet, SafeAreaView, View} from 'react-native';
 
 import Header from './components/Header';
+import { main } from './styles/style';
 
 
 export default function App() {
 
-  const [text, ]
 
   return (
     <SafeAreaView>
---
+++ scr/LearnPhysics/styles/style.js
@@ -0,0 +1,9 @@
+import { StyleSheet } from "react-native";
+
+
+export const main = StyleSheet.create({
+    text: {
+        color: 'blue',
+        
+    }
+})
