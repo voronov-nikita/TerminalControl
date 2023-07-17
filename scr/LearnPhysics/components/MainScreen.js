39cab0b97f867c93ad7a2cdcb3e7c16f4baa9ca5
voronov <114691383+voronov-nikita@users.noreply.github.com>
2023-07-17 23:19:51 +0300

    Update MainScreen.js

--- scr/LearnPhysics/components/MainScreen.js
+++ scr/LearnPhysics/components/MainScreen.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import { StyleSheet, View, Text} from 'react-native';
+import { StyleSheet, View, Text, Button} from 'react-native';
 
 
 export default function Main() {
   return (
     <View>
       <Text style={styles.text}> Learn Physics </Text>
+      <Button />
     </View>
   );
 }
