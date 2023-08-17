7e80e5b6e9ca81da30868879e8bf4c1636f5d29b
voronov <114691383+voronov-nikita@users.noreply.github.com>
2023-08-17 11:04:30 +0300

    add-new-functions-in-class-database-and-create-main-logic

--- lib/main.py
+++ lib/main.py
@@ -1,26 +1,37 @@
-from flask import Flask, jsonify,request
+from flask import Flask, jsonify, request
+from database import DataBase
 
 
+
+# <-------------------- MAIN LOGIC OF SERVER -------------------->
 app = Flask(__name__)
+database = DataBase("info.db")
+# create new table with data of users
+database.create_user_info()
 
 
-@app.route('/')
-def index():
-    data = {
-        'pin': 10,
-        'temperature': 30,
-        'humidity': 100
-    }
-    return jsonify(data)
+
+# POST: { id:value, timer: value }
+@app.route('/new_account', methods=['POST'])
+def create_new_account():
+    post_data = request.json
+    result = database.creacte_new_account(post_data)
+    
+    return result
 
 
-@app.route('/post', methods=['POST'])
-def post_request():
-    data = request.json
-    data['key1'] = 100
-    data['key2'] = 200
-    return jsonify(data)
+# POST: { login: value, password: value, email: value}
+@app.route('/new_game', methods=['POST'])
+def create_new_game():
+    post_data = request.json
+    user_data = database.get_user_info(post_data['id'])
+    database.create_new_game(user_data[0], user_data[3], post_data['timer'])
+    
+    return "Succesfully"
 
 
+
+# <------------ START SERVER ---------------->
 if __name__=="__main__":
+    
     app.run(debug=True, host='127.0.0.1', port=5000)
