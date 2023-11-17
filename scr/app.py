cbb959507a285402d27a0038ebce761549cd1e55
voronov <114691383+voronov-nikita@users.noreply.github.com>
2023-11-17 23:57:29 +0300

    Add UI file

--- scr/app.py
+++ scr/app.py
@@ -1,4 +1,5 @@
 from PyQt5.QtWidgets import QApplication, QMainWindow, QComboBox, QPushButton
+from PyQt5 import uic
 from PyQt5.QtGui import QIcon
 import subprocess
 import socket
@@ -12,12 +13,12 @@ class NetworkActions():
         self.networkip = socket.gethostbyname(socket.gethostname())
         print(self.networkip)
     
-    def network_scan(self) -> str:
+    def network_scan(self, host) -> str:
         '''
         Executes a command to fill in the arp table relative to the 
         ip address of the user who is making the request.
         '''
-        os.system(rf"for /L %a in (1,1,254) do @start /b ping 192.168.{0}.%a -n 2 > nul")
+        os.system(rf"for /L %a in (1,1,254) do @start /b ping {host}.%a -n 2 > nul")
         return subprocess.check_output(['arp', '-a'])
     
     def get_network(self) -> str:
@@ -28,18 +29,18 @@ class NetworkActions():
         arp table and further assembling this bacillus into a dictionary.
         '''
         return self.networkip
-
+        
 
 class App(QMainWindow):
     def __init__(self):
         super().__init__()
         
+        uic.loadUi("interface.ui", self)
         self.actions = NetworkActions()
         
         self.windowName = "Terminal Control"
         self.windowSize = (400, 500)
         self.windowIcon = ""
-        
         
         self.initUI()
         
@@ -47,22 +48,17 @@ class App(QMainWindow):
         self.setWindowTitle(self.windowName)
         self.setFixedSize(self.width(), self.height())
         
-        self.box = QComboBox(self)
-        self.box.addItems(["Zone 2", "Zone 3"])
-        
-        self.button = QPushButton(self)
-        self.button.setText("Connect")
-        self.button.clicked.connect(self.InfillArp)
-
-
-    def InfillArp(self):
-        print()
         
 def parsejson(file:str) -> dict:
-    return json.loads(file)
+    f = open(file, 'r')
+    ls = ''.join(f.readlines())
+    return json.loads(ls)
 
 
 if __name__=="__main__":
+    # ex = NetworkActions()
+    # d = parsejson("mac.json")
+    # ex.filltable(d)
     app = QApplication(sys.argv)
     ex = App()
     ex.show()
--- scr/logic.py
+++ scr/logic.py
@@ -7,10 +7,10 @@ def run_command_remotely(command, password, host):
 
 
 command_to_run = 'reboot'
-remote_password = 'qsc[;.'
+remote_password = '1234'
 ls = [218, 130, 142]
 # for i in ls:
-host = f"user@192.168.11.172"
+host = f"vnr@192.168.0.22"
 output = run_command_remotely(command_to_run, remote_password, host)
 
 print(output)
