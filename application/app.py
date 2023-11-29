8a1c9a4a0d99a455c0e0401e1c78d312fadbc475
voronov <114691383+voronov-nikita@users.noreply.github.com>
2023-11-29 18:46:50 +0300

    change ijn main files
    full delete logic.py
    change in app.py

--- application/app.py
+++ application/app.py
@@ -1,41 +1,14 @@
 from PyQt5.QtWidgets import QApplication, QMainWindow, QComboBox, QPushButton
-from PyQt5 import uic
 from PyQt5.QtGui import QIcon
-import subprocess
-import socket
 import json
 import sys
 import os
 
 
-class NetworkActions():
-    def __init__(self):
-        self.networkip = socket.gethostbyname(socket.gethostname())
-        print(self.networkip)
-    
-    def network_scan(self, host) -> str:
-        '''
-        Executes a command to fill in the arp table relative to the 
-        ip address of the user who is making the request.
-        '''
-        os.system(rf"for /L %a in (1,1,254) do @start /b ping {host}.%a -n 2 > nul")
-        return subprocess.check_output(['arp', '-a'])
-    
-    def get_network(self) -> str:
-        '''
-        Returns the value of the user's current ip address. 
-        
-        It is used mainly when creating an 
-        arp table and further assembling this bacillus into a dictionary.
-        '''
-        return self.networkip
-        
 
 class App(QMainWindow):
     def __init__(self):
         super().__init__()
-
-        self.actions = NetworkActions()
         
         self.windowName = "Terminal Control"
         self.windowSize = (400, 500)
@@ -45,9 +18,28 @@ class App(QMainWindow):
         
     def initUI(self):
         
-        uic.loadUi("interface.ui", self)
         self.setWindowTitle(self.windowName)
         self.setFixedSize(self.width(), self.height())
+        
+        
+class Zone3(MainLogic):
+    def __init__(self) -> None:
+        self.windowName:str = "Computer`s zone 3"
+        
+        self.initUI()
+        
+    def initUI(self):
+        pass
+    
+    
+class Zone5(MainLogic):
+    def __init__(self) -> None:
+        self.windowName:str = "Computer`s zone 5"
+        
+        self.initUI()
+        
+    def initUI(self):
+        pass
         
         
 def parsejson(file:str) -> dict:
